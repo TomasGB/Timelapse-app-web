1a96ba6a6cad23d84a15ba3d4cc8c4f3da22d129
Tomas Gomez Bermudez <g.btomas@hotmail.com>
2021-03-25 10:14:17 -0300

    started making the UI

---
+++ components/downloadButton.js
@@ -0,0 +1,20 @@
+const DownloadButton = () => {
+    return (
+        <div
+            style={{
+                padding: "10px 25px",
+                backgroundColor: "#e97c00",
+                borderRadius: "10px",
+                marginBottom: "15px",
+            }}>
+            <a
+                href="Proyecto Final PCI-Tomas Gomez Bermudez-Primer Cuatrimestre 2020 (correcion).rar"
+                target="_blank"
+                download>
+                Download here
+            </a>
+        </div>
+    );
+};
+
+export default DownloadButton;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,69 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import DownloadButton from "../components/downloadButton";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Automatic Timelapse Processor</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <main className={styles.main}>
+                <section>
+                    <h1 className={styles.title}>
+                        Automatic Timelapse Processor
+                    </h1>
+                    <p className={styles.description}>
+                        This Automatic Timelapse Processor is able to completely
+                        automate the process of making a timelapse, that
+                        includes taking the photos, apply gamma correction and
+                        histogram equalization to improve the quality of the
+                        photos and finally export the timelapse as an mp4 file
+                        with processing and another without.
+                    </p>
+                </section>
+                <section className={styles.section}>
+                    <h2 className={styles.subtitle}>User Interface</h2>
+                    <p className={styles.text}>
+                        With an intuitive user interface to facilitate user
+                        experience
+                    </p>
+                    <div className={styles.image}>
+                        <img
+                            src="imgs/timelapseUI.gif"
+                            alt="user-interface"
+                            width="400"
+                            height="400"
+                            style={{
+                                borderRadius: "5px",
+                            }}
+                        />
+                    </div>
+                    <h2 className={styles.subtitle}>
+                        No need of a professional camera
+                    </h2>
+                    <p className={styles.text}>
+                        This application is designed to be used with webcams so
+                        you can use whatever device you have,<br></br> of course
+                        if you have a professional camera you can use it as a
+                        webcam as well.
+                    </p>
+                    <div
+                        style={{
+                            width: "100%",
+                            height: "300px",
+                            backgroundColor: "red",
+                            marginBottom: "50px",
+                        }}>
+                        cameras icons
+                    </div>
+                </section>
+                <DownloadButton />
+            </main>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <footer className={styles.footer}></footer>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    );
 }
