8628cb52fff4d33042719ab961871de4cebebea4
Tomas Gomez Bermudez <g.btomas@hotmail.com>
2021-04-19 21:19:58 -0300

    updated with new version for download

--- components/downloadButton.js
+++ components/downloadButton.js
@@ -2,11 +2,11 @@ import styles from "../styles/DownloadBtn.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
 const DownloadButton = () => {
-    const CurrentVersion = "1.0.0";
+    const CurrentVersion = "1.2.0";
     return (
         <div className={styles.DownloadBtn}>
             <FontAwesomeIcon icon={faDownload} className={styles.icon} />
-            <a href="timelapsing v1.0.0.rar" target="_blank" download>
+            <a href="Timelapsing v1.2.0.rar" target="_blank" download>
                 <p
                     style={{
                         display: "flex",
